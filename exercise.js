d3bbbdd6181cb9083834f3452ac3e076740818f8
furhadkhan <furhadkhan@gmail.com>
2022-09-29 16:42:50 +0200

    add js functions exercise and html-css-lab

--- exercise.js
+++ exercise.js
@@ -1,1 +1,13 @@
-function sum()
+function sum(a, b) {
+var result = a + b;
+return result;
+}
+const result = sum();
+console.log(result);
+ 
+var waitThenRun = function () {
+     console.log('Hello!');
+     waitThenRun(function()) {
+         console.log('Goodbye!');
+     ) }
+}, 1500)
